dbbdb1203253f32e66ae1c3cac958dc437cd6359
root <gowtham@agiratech.com>
2017-09-15 17:21:45 +0530

    Api Tasks added:     1.Network connection checking     2.picasso and Glide

--- app/src/main/java/myapplication/com/sql/Main2Activity.java
+++ app/src/main/java/myapplication/com/sql/Main2Activity.java
@@ -1,12 +1,14 @@
 package myapplication.com.sql;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
@@ -18,16 +20,27 @@ public class Main2Activity extends AppCompatActivity {
     private EditText phone;
     private MyHelper MyHelper1;
     private SQLiteDatabase SQliteDb;
+    private AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         name=(EditText)findViewById(R.id.name);
         phone=(EditText)findViewById(R.id.surn);
+        b1 = (AppCompatButton) findViewById(R.id.but);
+
         MyHelper1=new MyHelper(Main2Activity.this,"STUDB",null,1);
         SQliteDb=MyHelper1.getWritableDatabase();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), RepoAct.class);
+                startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +64,10 @@ public class Main2Activity extends AppCompatActivity {
         while (c.moveToNext()){
             Toast.makeText(Main2Activity.this,c.getString(c.getColumnIndex("name")),Toast.LENGTH_LONG).show();
         }
+
+
+
     }
+
 
 }
---
+++ app/src/main/java/myapplication/com/sql/RepoAct.java
@@ -0,0 +1,65 @@
+package myapplication.com.sql;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.squareup.picasso.Picasso;
+
+public class RepoAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ImageView imageView = (ImageView) findViewById(R.id.thumb);
+        String imgUrl = "http://cdn.empireonline.com/jpg/80/0/0/1000/563/0/north/0/0/0/0/0/t/films/99861/images/rFtsE7Lhlc2jRWF7SRAU0fvrveQ.jpg";
+
+
+        Picasso.with(getApplicationContext())
+                .load(imgUrl)
+                .into(imageView);
+
+
+
+        setSupportActionBar(toolbar);
+        isNetworkConnected();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });}
+    private void isNetworkConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE); // 1
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo(); // 2
+        if(networkInfo!=null){
+            Toast.makeText(getApplicationContext(),"Network is enabled",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Network is not enabled",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    }
+
+
+
+
